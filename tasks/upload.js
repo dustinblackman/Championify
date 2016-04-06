4af4871fd14e6ec7688f9bba0d12b71a0c3eeec4
Dustin Blackman <dev@dustinblackman.com>
2016-04-06 08:28:39 -0400

    fix release task

--- tasks/upload.js
+++ tasks/upload.js
@@ -1,11 +1,11 @@
 import Promise from 'bluebird';
 import GitHubAPI from 'github';
-import fs from 'fs';
 import glob from 'glob';
 import gulp from 'gulp';
 import path from 'path';
 import R from 'ramda';
 
+const fs = Promise.promisifyAll(require('fs-extra'));
 const request = Promise.promisify(require('request'));
 const pkg = require('../package.json');
 
@@ -30,7 +30,8 @@ gulp.task('virustotal', function() {
             .then(R.prop('permalink'))
             .then(permalink => GLOBAL.vtReports[path.basename(file_path)] = permalink);
         });
-    });
+    })
+    .tap(() => console.log(GLOBAL.vtReports));
 });
 
 gulp.task('github-release', function(cb) {
@@ -45,52 +46,51 @@ gulp.task('github-release', function(cb) {
   const createRelease = Promise.promisify(github.releases.createRelease);
   const uploadAsset = Promise.promisify(github.releases.uploadAsset);
 
-  return fs.readFileAsync('./CHANGELOG.md', 'utf8')
-    .then(changelog => {
-      let body = changelog.split(/<a name="*.*.*" \/>/g)[1];
-      body += '\n\n## Virus Total Reports\n';
+  const changelog = fs.readFileSync('./CHANGELOG.md', 'utf8');
+  let body = changelog.split(/<a name="*.*.*" \/>/g)[1];
+  body += '\n\n## Virus Total Reports\n';
 
-      function formatTitle(name, link) {
-        return `[${name} | VirusTotal Report](${link})\n`;
-      }
+  function formatTitle(name, link) {
+    return `[${name} | VirusTotal Report](${link})\n`;
+  }
 
-      R.forEach(item => {
-        if (item.indexOf('Windows_Setup') > -1) body += formatTitle('Windows Setup', GLOBAL.vtReports[item]);
-        if (item.indexOf('.WIN.') > -1) body += formatTitle('Windows ZIP', GLOBAL.vtReports[item]);
-        if (item.indexOf('OSX') > -1) body += formatTitle('Mac/OSX', GLOBAL.vtReports[item]);
-        if (item.indexOf('asar') > -1) body += formatTitle('update.asar', GLOBAL.vtReports[item]);
-        if (item.indexOf('u_osx') > -1) body += formatTitle('u_osx.tar.gz', GLOBAL.vtReports[item]);
-        if (item.indexOf('u_win') > -1) body += formatTitle('u_win.tar.gz', GLOBAL.vtReports[item]);
-      }, R.keys(GLOBAL.vtReports));
+  R.forEach(item => {
+    if (item.indexOf('Windows_Setup') > -1) body += formatTitle('Windows Setup', GLOBAL.vtReports[item]);
+    if (item.indexOf('.WIN.') > -1) body += formatTitle('Windows ZIP', GLOBAL.vtReports[item]);
+    if (item.indexOf('OSX') > -1) body += formatTitle('Mac/OSX', GLOBAL.vtReports[item]);
+    if (item.indexOf('asar') > -1) body += formatTitle('update.asar', GLOBAL.vtReports[item]);
+    if (item.indexOf('u_osx') > -1) body += formatTitle('u_osx.tar.gz', GLOBAL.vtReports[item]);
+    if (item.indexOf('u_win') > -1) body += formatTitle('u_win.tar.gz', GLOBAL.vtReports[item]);
+  }, R.keys(GLOBAL.vtReports));
 
-      body += [
-        '',
-        '## How to Download:',
-        'Below you\'ll find the download section. If you\'re on Windows, your best bet it to select the "Windows Setup" to get yourself started with Championify. If you have trouble installing you can always try the ".zip" version.',
-        'For Mac, download the file labeled "OSX", extract the .zip, and you\'ll be good to go!'
-      ].join('\n');
+  body += [
+    '',
+    '## How to Download:',
+    'Below you\'ll find the download section. If you\'re on Windows, your best bet it to select the "Windows Setup" to get yourself started with Championify. If you have trouble installing you can always try the ".zip" version.',
+    'For Mac, download the file labeled "OSX", extract the .zip, and you\'ll be good to go!'
+  ].join('\n');
 
-      const create_release = {
-        owner: 'dustinblackman',
-        repo: 'Championify',
-        tag_name: pkg.version,
-        draft: true,
-        name: `Championify ${pkg.version}`,
-        body
-      };
+  const create_release = {
+    owner: 'dustinblackman',
+    repo: 'Championify',
+    tag_name: pkg.version,
+    draft: true,
+    name: `Championify ${pkg.version}`,
+    body
+  };
 
-      return createRelease(create_release).then(R.prop('release_id'));
-    })
-    .then(release_id => {
+  return createRelease(create_release)
+    .then(R.prop('id'))
+    .then(id => {
       return Promise.resolve(glob.sync('./releases/*'))
         .each(file_path => {
           console.log(`[GITHUB] Uploading: ${file_path}`);
           const upload_file = {
             owner: 'dustinblackman',
             repo: 'Championify',
-            id: release_id,
+            id,
             name: path.basename(file_path),
-            file_path
+            filePath: file_path
           };
           return uploadAsset(upload_file);
         });
