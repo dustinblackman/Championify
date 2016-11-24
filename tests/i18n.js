c0f971e352210a96ecc935ce827db99d2ea88f8d
Dustin Blackman <dev@dustinblackman.com>
2016-11-24 14:04:27 -0500

    disable locale tests for PRs

--- tests/i18n.js
+++ tests/i18n.js
@@ -9,28 +9,37 @@ const _source = JSON.parse(fs.readFileSync(path.join(__dirname, '../i18n/_source
 const keys_count = R.keys(_source).length;
 const locales = {};
 
-describe('i18n', () => {
-  before(() => {
-    const locales_files = glob.sync('./i18n/*.json');
-    locales_files.shift();
-    R.forEach(locale_path => {
-      const locale = path.basename(locale_path).replace('.json', '');
-      locales[locale] = JSON.parse(fs.readFileSync(path.join(__dirname, '..', locale_path)));
-    }, locales_files);
-  });
+function isPR() {
+  if (process.env.TRAVIS_PULL_REQUEST && process.env.TRAVIS_PULL_REQUEST !== 'false') return true;
+  if (process.env.APPVEYOR_PULL_REQUEST_NUMBER) return true;
+  return false;
+}
 
-  describe('each locale', () => {
-    it('should have the same length of keys as _source', () => {
-      R.forEach(locale => {
-        R.keys(locales[locale]).length.should.equal(keys_count);
-      }, R.keys(locales));
+if (!isPR()) {
+  describe('i18n', () => {
+    before(() => {
+      const locales_files = glob.sync('./i18n/*.json');
+      locales_files.shift();
+      R.forEach(locale_path => {
+        const locale = path.basename(locale_path).replace('.json', '');
+        locales[locale] = JSON.parse(fs.readFileSync(path.join(__dirname, '..', locale_path)));
+      }, locales_files);
     });
-    it('should contain the same keys as _source', () => {
-      R.forEach(key => {
+
+    describe('each locale', () => {
+      it('should have the same length of keys as _source', () => {
         R.forEach(locale => {
-          should.exist(locales[locale][key]);
+          R.keys(locales[locale]).length.should.equal(keys_count);
         }, R.keys(locales));
-      }, R.keys(_source));
+      });
+      it('should contain the same keys as _source', () => {
+        R.forEach(key => {
+          R.forEach(locale => {
+            should.exist(locales[locale][key]);
+          }, R.keys(locales));
+        }, R.keys(_source));
+      });
     });
   });
-});
+}
+
