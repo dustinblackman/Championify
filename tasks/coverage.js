090286d1cdb87602503f02a62b51377167372a82
Dustin Blackman <dev@dustinblackman.com>
2016-12-06 23:09:29 -0500

    fix aws keys

--- tasks/coverage.js
+++ tasks/coverage.js
@@ -14,8 +14,8 @@ const request = Promise.promisify(require('request'));
 const SkipCoverallsError = SuperError.subclass('SkipCoverallsError');
 
 aws.config.update({
-  accessKeyId: process.env.AWS_KEY,
-  secretAccessKey: process.env.AWS_TOKEN
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const s3 = Promise.promisifyAll(new aws.S3({
