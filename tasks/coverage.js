a07449132e22990116f65da1f247d2f872aee65a
Dustin Blackman <dev@dustinblackman.com>
2016-12-06 22:59:39 -0500

    rewrite coverage reporting

--- tasks/coverage.js
+++ tasks/coverage.js
@@ -1,6 +1,6 @@
 import Promise from 'bluebird';
 import aws from 'aws-sdk';
-import { exec } from 'child_process';
+import { execAsync } from './helpers';
 import gulp from 'gulp';
 import moment from 'moment';
 import open from 'open';
@@ -10,81 +10,65 @@ import SuperError from 'super-error';
 
 const fs = Promise.promisifyAll(require('fs-extra'));
 const request = Promise.promisify(require('request'));
-const travis = require(path.join(__dirname, 'helpers/travis.js')).default;
 
 const SkipCoverallsError = SuperError.subclass('SkipCoverallsError');
 
+aws.config.update({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_TOKEN
+});
 
-function _exec(cmd) {
-  return new Promise((resolve, reject) => {
-    return exec(cmd, function(err, stdout, stderr) {
-      if (stdout) console.log(stdout);
-      if (stderr) console.log(stderr);
+const s3 = Promise.promisifyAll(new aws.S3({
+  params: {Bucket: 'dustinblackman-championify-coverage'}
+}));
 
-      if (err) return reject(err);
-      resolve();
-    });
-  });
-}
 
 function _istanbul(report_type) {
-  return _exec(`${path.resolve('./node_modules/.bin/babel-istanbul')} report ${report_type}`);
+  return execAsync(`${path.resolve('./node_modules/.bin/babel-istanbul')} report ${report_type}`);
 }
 
-function fromTravis() {
-  aws.config.update({
-    accessKeyId: process.env.AWS_KEY,
-    secretAccessKey: process.env.AWS_TOKEN
-  });
-  let s3 = new aws.S3({
-    params: {Bucket: 'dustinblackman-travis-artifacts'}
-  });
-  s3 = Promise.promisifyAll(s3);
+function uploadCoverage() {
+  let commit = process.env.APPVEYOR_REPO_COMMIT;
+  let filename = 'appveyor.json';
+  if (process.env.TRAVIS_COMMIT) {
+    commit = process.env.TRAVIS_COMMIT;
+    filename = 'travis.json';
+  }
 
-  return s3.listObjectsAsync({Prefix: `dustinblackman/Championify/${process.env.APPVEYOR_BUILD_NUMBER}`})
+  console.log(`Uploading ${commit}/${filename}`);
+  return s3.uploadAsync({
+    Key: `${commit}/${filename}`,
+    Body: path.join(__dirname, '..', 'coverage/coverage.json')
+  });
+}
+
+function getCoverage() {
+  const commit = process.env.APPVEYOR_REPO_COMMIT || process.env.TRAVIS_COMMIT;
+  const coverage_path = path.join(__dirname, '..', 'coverage/coverage-other.json');
+  const converted_coverage = {};
+
+  return s3.listObjectsAsync({Prefix: commit})
     .then(data => {
-      let coverage_key;
-      R.forEach(obj => {
-        if (!coverage_key && obj.Key.indexOf('coverage.json') > -1) coverage_key = obj.Key;
-      }, data.Contents);
-      if (!coverage_key) throw new Error('Travis not ready, coverage file not found on S3.');
-
-      return s3.getObjectAsync({Key: coverage_key})
-        .then(R.prop('Body'))
-        .then(R.toString)
-        .then(JSON.parse);
-    });
+      if (data.Contents.length < 2) throw new Error('Missing second coverage file, skipping');
+      return Promise.map(data.Contents, entry => {
+        return s3.getObjectAsync({Key: entry.key})
+          .then(R.prop('Body'))
+          .then(R.toString)
+          .then(JSON.parse);
+      });
+    })
+    .each(coverage_json => {
+      R.forEach(coverage_data => {
+        let file = coverage_data.path.split('src')[1].replace(/\\/g, '/');
+        if (file[0] === '/') file = file.substring(1);
+        coverage_data.path = path.join(__dirname, '..', 'src', file);
+        converted_coverage[coverage_data.path] = coverage_data;
+      }, R.values(coverage_json));
+    })
+    .then(() => fs.writeFileAsync(coverage_path, JSON.stringify(converted_coverage), 'utf8'));
 }
 
-function fromAppveyor() {
-  const build_number = `${process.env.TRAVIS_BRANCH}-${process.env.TRAVIS_BUILD_NUMBER}`;
-  const headers = {
-    Authorization: `Bearer ${process.env.APPVEYOR_KEY}`,
-    'Content-type': 'application/json'
-  };
-  const options = {
-    url: `https://ci.appveyor.com/api/projects/dustinblackman/Championify/build/${build_number}`,
-    json: true,
-    headers
-  };
-
-  return request(options)
-    .then(R.prop('body'))
-    .then(build_data => {
-      if (!build_data.build || build_data.build.jobs) throw new Error('Appveyor not ready, job not found.');
-      const job_data = R.find(R.propEq('name', 'Platform: x86'))(build_data.build.jobs);
-      if (!job_data || job_data.status !== 'success') return;
-
-      const options = {
-        url: `https://ci.appveyor.com/api/buildjobs/${job_data.jobId}/artifacts/coverage/coverage.json`,
-        json: true,
-        headers
-      };
-      return request(options).then(R.prop('body'));
-    });
-}
-
-function onCoveralls() {
+function checkIfSubmitted() {
   const commit = process.env.APPVEYOR_REPO_COMMIT || process.env.TRAVIS_COMMIT;
   return request(`https://coveralls.io/builds/${commit}.json`)
     .then(res => {
@@ -92,64 +76,26 @@ function onCoveralls() {
     });
 }
 
-function coverageDownload() {
-  let getCoverage;
-  if (process.env.APPVEYOR) {
-    getCoverage = fromTravis;
-  } else if (process.env.TRAVIS) {
-    getCoverage = fromAppveyor;
-  } else {
-    throw new SkipCoverallsError('Not on CI');
-  }
-
-  return getCoverage()
-    .then(coverage_json => {
-      if (!coverage_json) throw new SkipCoverallsError('Other CI is not ready yet, skipping...');
-
-      const converted_coverage = {};
-      R.forEach(coverage_data => {
-        let file = coverage_data.path.split('src')[1].replace(/\\/g, '/');
-        if (file[0] === '/') file = file.substring(1);
-        coverage_data.path = path.join(__dirname, '..', 'src', file);
-        converted_coverage[coverage_data.path] = coverage_data;
-      }, R.values(coverage_json));
-
-      const coverage_path = path.join(__dirname, '..', 'coverage/coverage-other.json');
-      return fs.writeFileAsync(coverage_path, JSON.stringify(converted_coverage), 'utf8');
-    });
-}
-
-function coverallsSetup() {
+// TODO: Stop executing command line, tap in to coveralls source instead.
+function runCoveralls() {
   // If Travis
   if (process.env.TRAVIS) {
     const cmd = 'cat ./coverage/lcov.info | ./node_modules/.bin/coveralls';
     return fs.writeFileAsync('./coveralls.sh', cmd, 'utf8')
-      .then(() => _exec('bash ./coveralls.sh'));
+      .then(() => execAsync('bash ./coveralls.sh'));
   }
 
   // If Appveyor
-  const params = {
-    url: 'https://api.travis-ci.org/repos/dustinblackman/Championify/builds',
-    method: 'GET'
-  };
+  const cmds = [
+    '@echo off',
+    `SET COVERALLS_RUN_AT=${moment().format('YYYY-MM-DDTHH:mm:ssZ')}`,
+    `SET COVERALLS_GIT_BRANCH=${process.env.APPVEYOR_REPO_BRANCH}`,
+    `SET COVERALLS_GIT_COMMIT=${process.env.APPVEYOR_REPO_COMMIT}`,
+    'type .\\coverage\\lcov.info | .\\node_modules\\.bin\\coveralls'
+  ];
 
-  return travis(params)
-    .then(body => {
-      const build_number = process.env.APPVEYOR_BUILD_NUMBER.toString();
-      const build = R.find(R.propEq('number', build_number))(body.builds);
-      if (!build) throw new Error(`Could not find travis build number: ${build_number}`);
-
-      const cmds = [
-        '@echo off',
-        `SET COVERALLS_RUN_AT=${moment().format('YYYY-MM-DDTHH:mm:ssZ')}`,
-        `SET COVERALLS_SERVICE_JOB_ID=${build.job_ids[0]}`,
-        `SET COVERALLS_GIT_BRANCH=${process.env.APPVEYOR_REPO_BRANCH}`,
-        `SET COVERALLS_GIT_COMMIT=${process.env.APPVEYOR_REPO_COMMIT}`,
-        'type .\\coverage\\lcov.info | .\\node_modules\\.bin\\coveralls'
-      ];
-      return fs.writeFileAsync('./coveralls.bat', cmds.join('\n'), 'utf8');
-    })
-    .then(() => _exec('.\\coveralls.bat'));
+  return fs.writeFileAsync('./coveralls.bat', cmds.join('\n'), 'utf8')
+   .then(() => execAsync('.\\coveralls.bat'));
 }
 
 gulp.task('istanbul-coverage', function() {
@@ -157,7 +103,7 @@ gulp.task('istanbul-coverage', function() {
 });
 
 gulp.task('istanbul-instrument', function() {
-  return _exec(`${path.resolve('./node_modules/.bin/babel-istanbul')} instrument -o src-cov src`);
+  return execAsync(`${path.resolve('./node_modules/.bin/babel-istanbul')} instrument -o src-cov src`);
 });
 
 gulp.task('istanbul-cleanup', function() {
@@ -165,15 +111,12 @@ gulp.task('istanbul-cleanup', function() {
 });
 
 gulp.task('coveralls', function() {
-  if ((process.env.APPVEYOR && !process.env.AWS_KEY) || (process.env.TRAVIS && !process.env.APPVEYOR_KEY)) {
-    console.log('Coveralls is disabled for pull requests');
-    return Promise.resolve();
-  }
-
-  return onCoveralls()
-    .then(coverageDownload)
+  // TODO: Check if a check is needed to disable during PRs
+  return checkIfSubmitted()
+    .then(uploadCoverage)
+    .then(getCoverage)
     .then(() => _istanbul('lcov text-summary'))
-    .then(coverallsSetup)
+    .then(runCoveralls)
     .catch(err => {
       console.log(err.stack || err);
     });
--- tasks/helpers.js
+++ tasks/helpers.js
@@ -1,5 +1,5 @@
 import Promise from 'bluebird';
-import { spawn } from 'child_process';
+import { exec, spawn } from 'child_process';
 
 export function spawnAsync(cmd, args) {
   return new Promise((resolve, reject) => {
@@ -8,6 +8,18 @@ export function spawnAsync(cmd, args) {
     proc.stderr.on('data', data => console.log(data.toString()));
     proc.on('close', code => {
       if (code !== 0) return reject(new Error(`${cmd} ${args.join(' ')} exited with code ${code}}`));
+      resolve();
+    });
+  });
+}
+
+export function execAsync(cmd) {
+  return new Promise((resolve, reject) => {
+    return exec(cmd, function(err, stdout, stderr) {
+      if (stdout) console.log(stdout);
+      if (stderr) console.log(stderr);
+
+      if (err) return reject(err);
       resolve();
     });
   });
