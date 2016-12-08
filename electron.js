5be9fdf13d30bb40db127adda64cea41780b8a0f
Dustin Blackman <dev@dustinblackman.com>
2016-12-07 21:48:26 -0500

    cleanup

--- electron.js
+++ electron.js
@@ -1,5 +1,4 @@
 import { app, BrowserWindow } from 'electron';
-import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import R from 'ramda';
@@ -9,26 +8,13 @@ if (require('electron-squirrel-startup')) app.quit();
 
 const dev_enabled = process.env.NODE_ENV === 'development' || fs.existsSync('./dev_enabled') || fs.existsSync(path.join(__dirname, '..', 'dev_enabled'));
 
-
-let preference_dir;
-if (process.platform === 'darwin') {
-  preference_dir = path.join(process.env.HOME, 'Library/Application Support/Championify/');
-} else {
-  preference_dir = path.join(process.env.APPDATA, 'Championify');
-}
-
-if (R.contains('--win-major', process.argv)) {
-  const update_file = path.join(preference_dir, 'update_major.bat');
-  exec(`START "" "${update_file}"`, {cwd: path.join(process.cwd(), '..')});
-}
-
-let mainWindow = null;
-app.on('window-all-closed', function() {
+let main_window = null;
+app.on('window-all-closed', () => {
   app.quit();
 });
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({
+app.on('ready', () => {
+  main_window = new BrowserWindow({
     fullscreen: false,
     width: 450,
     height: 670,
@@ -39,14 +25,14 @@ app.on('ready', function() {
     title: 'Championify'
   });
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  if (dev_enabled) mainWindow.openDevTools({detach: true});
+  main_window.loadURL(`file://${__dirname}/index.html`);
+  if (dev_enabled) main_window.openDevTools({detach: true});
 
-  mainWindow.webContents.on('did-finish-load', function() {
-    if (!R.contains('--autorun', process.argv)) return mainWindow.show();
+  main_window.webContents.on('did-finish-load', () => {
+    if (!R.contains('--autorun', process.argv)) return main_window.show();
   });
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
+  main_window.on('closed', () => {
+    main_window = null;
   });
 });
