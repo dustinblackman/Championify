00f911722e53300437e8c7f912b0071280676404
webdo <info@911concept.com>
2017-01-15 22:41:05 -0500

    Using New launcher when available

--- src/path_manager.js
+++ src/path_manager.js
@@ -17,10 +17,13 @@ function findInstallPath() {
   if (process.platform === 'darwin') {
     if (fs.existsSync('/Applications/League of Legends.app')) {
       return this.setInstallPath(null, '/Applications/League of Legends.app/', 'Contents/LoL/Config/Champions/');
-    } else if (fs.existsSync(`${user_home}/Applications/League of Legends.app`)) {
-      return this.setInstallPath(null, `${user_home}/Applications/League of Legends.app/`, 'Contents/LoL/Config/Champions/');
+    } else if (fs.existsSync('${user_home}/Applications/League of Legends.app')) {
+      return this.setInstallPath(null, '${user_home}/Applications/League of Legends.app/', 'Contents/LoL/Config/Champions/');
     }
-  } else if (fs.existsSync('C:/Riot Games/League Of Legends/lol.launcher.exe')) {
+  } else if (fs.existsSync('C:/Riot Games/League Of Legends/LeagueClient.exe')) {
+    return this.setInstallPath(null, 'C:/Riot Games/League Of Legends/', 'Config/Champions/', 'LeagueClient.exe');
+  }
+  else if (fs.existsSync('C:/Riot Games/League Of Legends/lol.launcher.exe')) {
     return this.setInstallPath(null, 'C:/Riot Games/League Of Legends/', 'Config/Champions/', 'lol.launcher.exe');
   }
 }
@@ -52,17 +55,21 @@ function checkInstallPath(selected_path, done) {
     }
   } else {
     const default_path = path.join(selected_path, 'lol.launcher.exe');
+	const new_launcher_path = path.join(selected_path, 'LeagueClient.exe');
     const garena_check_one = path.join(selected_path, 'lol.exe');
     const garena_check_two = glob.sync(path.join(selected_path, 'LoL*Launcher.exe'))[0];
 
-    if (fs.existsSync(default_path)) {
+    if (fs.existsSync(new_launcher_path)) {
+      done(null, selected_path, 'Config/Champions/', path.basename(new_launcher_path));
+    }else if (fs.existsSync(default_path)) {
       done(null, selected_path, 'Config/Champions/', path.basename(default_path));
     } else if (fs.existsSync(garena_check_one)) {
       done(null, selected_path, 'Game/Config/Champions/', path.basename(garena_check_one));
     } else if (garena_check_two) {
       let garena_version = path.basename(glob.sync(path.join(selected_path, 'GameData/Apps/*'))[0]);
-      done(null, selected_path, `GameData/Apps/${garena_version}/Game/Config/Champions/`, path.basename(garena_check_two));
-    } else {
+      done(null, selected_path, 'GameData/Apps/${garena_version}/Game/Config/Champions/', path.basename(garena_check_two));
+    
+	}	else {
       done(new Error('Path not found'), selected_path);
     }
   }
@@ -89,7 +96,7 @@ function setInstallPath(path_err, install_path, champ_path, executable) {
   }
   function foundLeague() {
     $('#input_msg').addClass('green');
-    $('#input_msg').text(`${T.t('found')} League of Legends!`);
+    $('#input_msg').text('${T.t('found')} League of Legends!');
     return enableBtns();
   }
 
