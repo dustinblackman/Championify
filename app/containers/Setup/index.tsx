fd752cac60fbe22865841ba5d34daa53090f9ca2
Dustin Blackman <dev@dustinblackman.com>
2017-11-24 21:03:07 -0500

    wip

--- app/containers/Setup/index.tsx
+++ app/containers/Setup/index.tsx
@@ -1,11 +1,23 @@
 import React = require("react");
+import { connect } from "react-redux";
 import Select from "react-select";
 import { Button, Col, Container, Input, InputGroup, InputGroupButton, Row } from "reactstrap";
+import { bindActionCreators } from "redux";
+
+import { ReducersState } from "../../store/reducers";
+import { sourceSelectActions, SourceSelectOption, SourceSelectProps } from "../../store/reducers/source-select";
 
 import * as styles from "./styles.scss";
 
-export default class Setup extends React.Component {
+type Props = SourceSelectProps;
+
+class Setup extends React.Component<Props, {}> {
+  handleSourceSelect = (selected: SourceSelectOption[]) => {
+    this.props.changeSources(selected);
+  }
+
   render() {
+    // TODO Replace.
     const select_options = [
       { value: "one", label: "One" },
       { value: "two", label: "Two" }
@@ -24,7 +36,10 @@ export default class Setup extends React.Component {
         <Row className={styles.top_padding}>
           <Col>
             <Select
-              name="form-field-name"
+              name="source-selection"
+              value={this.props.sources}
+              multi={true}
+              onChange={this.handleSourceSelect}
               options={select_options}
             />
           </Col>
@@ -33,3 +48,8 @@ export default class Setup extends React.Component {
     );
   }
 }
+
+export default connect(
+  (state: ReducersState) => ({sources: state.source_select.sources}),
+  (dispatch) => bindActionCreators(sourceSelectActions, dispatch)
+)(Setup);
--- app/store/index.ts
+++ app/store/index.ts
@@ -2,8 +2,10 @@ import { createStore as _createStore } from "redux";
 
 import reducers from "./reducers";
 
+const dev_tools = window && (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__();
+
 export default function createStore() {
-  const store = _createStore(reducers);
+  const store = _createStore(reducers, dev_tools);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
